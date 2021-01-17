2ea3acbf27880ca6dc0c2ced60f780d0754cfe0c
francisco <fran.mazzoni@gmail.com>
2021-01-17 16:41:31 -0300

    remove tailwind

---
+++ src/components/MovieDetails.tsx
@@ -0,0 +1,11 @@
+const MovieDetails = () => {
+  return (
+    <div className="py-1 px-2 max-w-xl grid grid-rows-2 grid-flow-col gap-2 w-146">
+      <div>Movie Title</div>
+      <div>Movie Desc </div>
+      <div className="row-span-2">* * * * *</div>
+    </div>
+  );
+};
+
+export default MovieDetails;
---
+++ src/components/MovieList.tsx
@@ -0,0 +1,16 @@
+import MovieDetails from "./MovieDetails";
+
+const MovieList = () => {
+  return (
+    <div>
+      <MovieDetails />
+      <MovieDetails />
+      <MovieDetails />
+      <MovieDetails />
+      <MovieDetails />
+      <MovieDetails />
+    </div>
+  );
+};
+
+export default MovieList;
